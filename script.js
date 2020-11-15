b6a66f2632e014b23052e57237ee1b15843d0046
Sam Sheets <sam.sheets94@gmail.com>
2020-11-15 11:42:26 -0700

    JSON data displayed in console and DOM

---
+++ script.js
@@ -0,0 +1,37 @@
+function getDogImages(inputValue) {
+    console.log($('#numberOfDogs').val());
+    inputValue = $('#numberOfDogs').val();
+    fetch(`https://dog.ceo/api/breeds/image/random/${inputValue}`)
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson))
+    .catch(e => alert('Something went wrong. Please try again.'));
+  }
+
+  function displayResults(responseJson) {
+      console.log(responseJson);
+      let arrayOfImg = responseJson.message;
+      $('.results-img').html(getImages(arrayOfImg));
+      $('.results').removeAttr('hidden');
+  }
+
+  function getImages(arrayOfImg) {
+      let imagesToReturn = '';
+      for (let i = 0; i < arrayOfImg.length; i++) {
+          imagesToReturn += `<img src="${arrayOfImg[i]}" class="js-results-img">`;
+      }
+      console.log(imagesToReturn);
+      return imagesToReturn;
+  }
+  
+  function handleSubmitButton() {
+    $('form').submit(event => {
+      event.preventDefault();
+      getDogImages();
+    });
+  }
+
+  
+  $(function() {
+    console.log('App loaded! Waiting for submit!');
+    handleSubmitButton();
+  })
